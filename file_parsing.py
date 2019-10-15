a484a4bec3eaa8648a83f1008857e79121cd9c41
MateuszKutyna <kutyna.mateusz1998@gmail.com>
2019-10-15 18:17:14 +0200

    final version

--- file_parsing.py
+++ file_parsing.py
@@ -1,38 +1,36 @@
-import pandas as pd
 import re
 import numpy as np
 import os
 
 
-def edit_array(arr, rows):
-    matrix = [[0 for x in range(2)] for y in range(rows)]
-    i = 0
-    while i < rows:
-        j = 0
-        while j < 2:
-            matrix[i][j] = arr[j][i]
-            j += 1
-        i += 1
-    return matrix
-
-
 def pars(file_name):
     line = []
+    line1 = []
     names_of_files = []
-    final_result = []
     if os.path.isfile(file_name):
+        # Open file and get dependency
         with open(file_name) as file:
             for x in file:
                 if x.startswith('import') | x.startswith('from'):
                     line.append(x)
-        # print(line)
+
         for l in line:
-            if re.search("from (.*) import", l) is not None:
-                names_of_files.append(re.search("from (.*) import", l).group(1))
+            if re.search("as (.*)\n", l) is not None:
+                names_of_files.append(re.search("as (.*)\n", l).group(1))
             elif re.search("import (.*)", l) is not None:
                 names_of_files.append(re.search("import (.*)\n", l).group(1))
+        # Create dictionary of a dependencies
+        unique_set = list(set(names_of_files))
+        dictionary = dict((k, 0) for k in unique_set)
+        # Count occurrence of dependencies
+        with open(file_name) as file:
+            for x in file:
+                line1.append(re.findall(r"[\w]+", x))
+            for x in line1:
+                for y in x:
+                    if y in dictionary:
+                        dictionary[y] = dictionary[y] + 1
+        # Convert dictionary to list of tuples
+        final_result = [(k, v) for k, v in dictionary.items()]
 
-        final_result = np.array(np.unique(names_of_files, return_counts=True))
-
-        return edit_array(final_result, len(final_result[0]))
-
+        return final_result
