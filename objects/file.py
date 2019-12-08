d60ca9528ef5ac608dd9a0aca38e6838070f2821
MateuszKutyna <kutyna.mateusz1998@gmail.com>
2019-12-08 15:21:17 +0100

    ready to go

--- objects/file.py
+++ objects/file.py
@@ -1,10 +1,16 @@
-import sys
-class File:
+import parsing.file_parsing
+import os
 
+
+class File:
     ID = 0
 
     def __init__(self, name):
         self.name = name
-        self.size = 0    #todo
+        self.size = os.stat(name).st_size
         self.ID = File.ID
+        self.imports = parsing.file_parsing.get_imports(name)
+        self.functions_def = parsing.file_parsing.get_function_def(name)
+        self.functions_calls = parsing.file_parsing.get_function_calls(name)
+
         File.ID += 1
