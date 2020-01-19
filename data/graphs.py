88e886bd24cbc694394a1f4ac4b4a09839111564
meytn <szifugupek4>
2020-01-19 19:40:53 +0100

    Small update with graphs

--- data/graphs.py
+++ data/graphs.py
@@ -1,14 +1,20 @@
 from matplotlib import pyplot as plt
 from objects import project
 import networkx as nx
-from matplotlib import style
-style.use('dark_background')
+
 
 def draw_modules_graph(proj: project.Project):
     fig, ax = plt.subplots()
     graph = nx.DiGraph()
     for module in proj.modules:
         graph.add_node(module.name)
+    for module in proj.modules:
+        for file in module.files:
+            pass
+
+
+
+
     nx.draw(graph, ax=ax, with_labels=True)
     return fig
 
@@ -17,10 +23,24 @@ def draw_files_graph(proj: project.Project):
     fig, ax = plt.subplots()
     graph = nx.DiGraph()
     for module in proj.modules:
-       print("Module: " + module.name)
-       print(module.files)
-       for file in module.files:
-           print("file: " + file.name)
-           graph.add_node(file.name)
+        for file in module.files:
+            if module.name == "global_scope":
+                graph.add_node(file.name)
+            else:
+                graph.add_node("%s.%s" % (module.name, file.name))
+
+    nx.draw(graph, ax=ax, with_labels=True)
+    return fig
+
+def draw_function_graph(proj: project.Project):
+    fig, ax = plt.subplots()
+    graph = nx.DiGraph()
+    for module in proj.modules:
+        for file in module.files:
+            if module.name == "global_scope":
+                graph.add_node(file.name)
+            else:
+                graph.add_node("%s.%s" % (module.name, file.name))
+
     nx.draw(graph, ax=ax, with_labels=True)
     return fig
