624f4e55ad17c830e2fc2223d4708baea62a21c5
meytn <szifugupek4>
2020-01-19 19:40:15 +0100

    Updated object definitions

--- objects/file.py
+++ objects/file.py
@@ -5,17 +5,18 @@ import os
 class File:
     ID = 0
 
-    def __init__(self, name, module):
-        self.name = name
+    def __init__(self, path, module):
+        self.path = path
+        self.name = os.path.basename(path)
         module_name = module.name
         if module_name == 'global_scope':
             module_name = '.'
         self.module = module
-        self.size = os.stat(os.path.join(module_name, name)).st_size
+        self.size = os.stat(self.path).st_size
         self.ID = File.ID
-        if ".py" in name:
-            self.imports = parsing.file_parsing.get_imports(os.path.join(module_name, name))
-            self.functions_def = parsing.file_parsing.get_function_def(os.path.join(module_name, name))
-            self.functions_calls = parsing.file_parsing.get_function_calls(os.path.join(module_name, name))
+        if ".py" in self.name:
+            self.imports = parsing.file_parsing.get_imports(os.path.join(module_name, self.path))
+            self.functions_def = parsing.file_parsing.get_function_def(os.path.join(module_name, self.path))
+            self.functions_calls = parsing.file_parsing.get_function_calls(os.path.join(module_name, self.path))
 
         File.ID += 1
--- objects/project.py
+++ objects/project.py
@@ -1,5 +1,3 @@
-import data.data
-import parsing.file_parsing
 import objects.module
 import os
 
@@ -7,23 +5,20 @@ import os
 class Project:
     def __init__(self, name, root_dir):
         self.name = name
+        self.path = root_dir
         self.modules = []
-        self.list_modules = pars_for_modules(root_dir)
-        for m in self.list_modules:
-            self.modules.append(objects.module.Module(m, self))
+        self.pars_for_modules()
 
-def pars_for_modules(root_dir):
-    list_of_modules = ['global_scope']
+    def pars_for_modules(self):
+        self.modules.append(objects.module.Module('global_scope', self))
 
-    for r, d, f in os.walk(root_dir):
-        if '.git' not in r:
-            if 'venv' not in r:
-                for mod in d:
-                    if '.git' not in mod:
-                        if 'venv' not in mod:
-                            if '__pycache__' not in mod:
-                                if '.idea' not in mod:
-                                    if 'inspectionProfiles' not in mod:
-                                        list_of_modules.append(mod)
-
-    return list_of_modules
+        for root, dirs, files in os.walk(self.path):
+            if not os.path.basename(os.path.normpath(root)) == '.':
+                files = [f for f in files if not f[0] == '.']
+                dirs[:] = [d for d in dirs if not d[0] == '.']
+                if "__init__.py" in files:
+                    self.modules.append(objects.module.Module(root, self))
+                else:
+                    for file in files:
+                        if file.endswith(".py"):
+                            self.modules[0].add_file(os.path.join(root, file))
