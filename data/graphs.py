dc76bb3eff647849637bb7abfd06adc346796d0c
meytn <szifugupek4>
2020-01-20 21:21:09 +0100

    Added drawing files and function graph

--- data/graphs.py
+++ data/graphs.py
@@ -9,11 +9,8 @@ def draw_modules_graph(proj: project.Project):
     for module in proj.modules:
         graph.add_node(module.name)
     for module in proj.modules:
-        for file in module.files:
-            pass
-
-
-
+        for dependency in module.get_internal_dependencies():
+            graph.add_edge(module.name, dependency)
 
     nx.draw(graph, ax=ax, with_labels=True)
     return fig
@@ -22,25 +19,24 @@ def draw_modules_graph(proj: project.Project):
 def draw_files_graph(proj: project.Project):
     fig, ax = plt.subplots()
     graph = nx.DiGraph()
-    for module in proj.modules:
-        for file in module.files:
-            if module.name == "global_scope":
-                graph.add_node(file.name)
-            else:
-                graph.add_node("%s.%s" % (module.name, file.name))
-
+    for file in proj.get_files():
+        graph.add_node(file.name)
+    for file in proj.get_files():
+        for dependency in file.get_internal_dependencies():
+            graph.add_edge(file.name, f'{dependency.split(".")[-1]}.py')
     nx.draw(graph, ax=ax, with_labels=True)
     return fig
 
 def draw_function_graph(proj: project.Project):
     fig, ax = plt.subplots()
     graph = nx.DiGraph()
-    for module in proj.modules:
-        for file in module.files:
-            if module.name == "global_scope":
-                graph.add_node(file.name)
-            else:
-                graph.add_node("%s.%s" % (module.name, file.name))
+    for fun in proj.get_functions():
+        if fun.name != "__init__":
+            graph.add_node(fun.name)
+    for fun in proj.get_functions():
+        if fun.name != "__init__":
+            for dependency in fun.get_internal_dependencies():
+                graph.add_edge(fun.name, dependency)
 
     nx.draw(graph, ax=ax, with_labels=True)
     return fig
